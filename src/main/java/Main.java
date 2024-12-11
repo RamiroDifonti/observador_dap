b4d41d6d3c8e8c33fb3fd3f3c18a87b4a1cdb930
PedroHdez18 <alu0101423944@ull.edu.es>
2024-12-11 16:24:26 +0000

    modificando main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,38 +17,5 @@ public class Main {
     public static void main(String[] args) {
         new LoginFrame();
     }
-    public void addUser(String[] args) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        Client test = new ConcretClient("testMobile");
-        Subject mobiles = new MobilesSubject();
-/*        Subject laptops = new LaptopsSubject();
-        Subject cpus = new CPUsSubject();*/
-        mobiles.addObserver(test);
-        try {
-            // Leer el archivo JSON
-            File file = new File("src/main/resources/data.json");
-            Map<String, List<Product>> catalog = objectMapper.readValue(
-                    file,
-                    new TypeReference<Map<String, List<Product>>>() {}
-            );
 
-            // Imprimir datos para verificar
-            System.out.println("Laptops:");
-            catalog.get("laptops").forEach(laptop ->
-                    System.out.println(laptop.getName() + " - $" + laptop.getPrice()));
-
-            // Si estas mirando esta parte del código la modifiqué para que funcionará el observador
-            // hay que quitarlo antes de la versión final...
-            System.out.println("\nMóviles:");
-            catalog.get("mobiles").forEach(mobile ->
-                    mobiles.addProduct(mobile));
-
-            System.out.println("\nCPUs:");
-            catalog.get("cpus").forEach(cpu ->
-                    System.out.println(cpu.getName() + " - $" + cpu.getPrice()));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
