010a0ef782e1a4bd0668bd064f6eb4fd4160e3b4
PedroHdez18 <alu0101423944@ull.edu.es>
2024-12-11 16:06:07 +0000

    creando clase LaptopsSubject

---
+++ src/main/java/subject/LaptopsSubject.java
@@ -0,0 +1,10 @@
+package subject;
+
+import utils.Product;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LaptopsSubject extends Subject {
+    private List<Product> _products = new ArrayList<>();
+    private Product _lastProduct;
+}
