9acdecebfb0f0cc9a6a5c8c342663b4a0b635390
PedroHdez18 <alu0101423944@ull.edu.es>
2024-12-12 01:03:36 +0000

    eliminando imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,4 @@
 import gui.LoginFrame;
-import observer.Client;
-import observer.ConcretClient;
-import subject.MobilesSubject;
-import subject.Subject;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import utils.Product;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
 public class Main {
 
